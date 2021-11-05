d2da6450c7f24725675a152368208d7a3262b657
2youyou2 <501251991@qq.com>
2021-11-05 19:20:16 +0800

    refine gbuffer format

--- cocos/core/pipeline/deferred/deferred-pipeline.ts
+++ cocos/core/pipeline/deferred/deferred-pipeline.ts
@@ -274,22 +274,17 @@ export class DeferredPipeline extends RenderPipeline {
         const data: DeferredRenderData = this._pipelineRenderData = new DeferredRenderData();
         const sceneData = this.pipelineSceneData;
         for (let i = 0; i < 4; ++i) {
-            let format = Format.RGBA8;
+            let format = Format.RGBA16F;
 
             // position need most precision for reflection
             if (i === 1) {
                 format = Format.RGBA32F;
             }
-            // normal & emission
-            // emission stored hdr lightmap info, need more precision
-            else if (i === 2 || i === 3) {
-                format = Format.RGBA16F;
-            }
 
             data.gbufferRenderTargets.push(device.createTexture(new TextureInfo(
                 TextureType.TEX2D,
                 TextureUsageBit.COLOR_ATTACHMENT | TextureUsageBit.SAMPLED,
-                Format.RGBA16F, // positions & normals need more precision
+                format, // positions & normals need more precision
                 this._width * sceneData.shadingScale,
                 this._height * sceneData.shadingScale,
             )));
